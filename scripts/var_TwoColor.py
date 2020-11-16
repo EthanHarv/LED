60b49a817cd3546b84664a3925cb8483bee8ca91
EthanHarv <github@ethanharv.com>
2020-11-15 23:43:56 -0600

    More variable functionality - Docs coming

--- scripts/var_TwoColor.py
+++ scripts/var_TwoColor.py
@@ -1,4 +1,4 @@
-#color,color
+#color,color,int_bound_1_300
 
 import board
 import neopixel
@@ -9,7 +9,7 @@ def run(pixels, stop, var):
 	split = var.split(',')
 	print(split)
 	for x in range(0,299):
-		if x<149:
+		if x<int(split[6]):
 			pixels[x] = ((int(split[0]), int(split[1]), int(split[2])))
 		else:
 			pixels[x] = ((int(split[3]), int(split[4]), int(split[5])))
--- static/js/led_buttons.js
+++ static/js/led_buttons.js
@@ -22,10 +22,23 @@ function populateVars(vars)
   $(".vars").html("");
   for (i=0; i<vars.length; i++)
   {
-    if (vars[i] == "color") // TODO: Add more available variables
+    if (vars[i] == "color") // TODO: Add "note"/identifier/comment feature. I.e. "First Color:"
     {
       $(".vars").append("<input class='varINPUT varcolor' id='colorinput" + i + "' value='rgb(0,255,0)' data-jscolor='{previewSize:60}'><br>");
       jscolor.install()
+    }
+    if (vars[i].startsWith("int"))
+    {
+      var split = vars[i].split("_");
+      if (split.length == 1) // Defaults to "Unbounded Text Input"
+      {
+        $(".vars").append("<input onkeypress='return isNumberKey(event)' class='varINPUT varint' id='intinput" + i + "'><br>"); // Important that we just use isNumberKey for unbounded
+      }
+      if (split[1] == "bound" ) // Attr low, high
+      {
+        $(".vars").append("<input onkeypress='return valBoundInput(event)' class='varINPUT varint' id='intinput" + i + "' low=" + split[2] + " high=" + split[3] + "><br>");
+      };
+      // TODO: Add slider
     }
   }
   $(".vars").append("<button class='send'>Send</button>")
@@ -34,15 +47,20 @@ function populateVars(vars)
 $(document).on("click", ".send", function()
 {
   fVars = "";
+  send = true;
   $(".varINPUT").each(function(i, obj)
   {
     if ($(obj).hasClass('varcolor')){
       c = document.querySelector("#" + $(obj).attr('id')).jscolor.channels;
       fVars += Math.floor(c.r) + "," + Math.floor(c.g) + "," + Math.floor(c.b);
     };
+    if ($(obj).hasClass('varint')){
+      if ($(obj)[0].value == "") { send = false; }
+      fVars += $(obj)[0].value;
+    };
     fVars += ","
   });
-  $.get("LED", { set: "var_" + lastFile, vars: fVars });
+  if (send) { $.get("LED", { set: "var_" + lastFile, vars: fVars }); };
 });
 
 
@@ -57,3 +75,26 @@ $(".dropbtn").click(function()
     $(".dropdown-content").attr("style", "display:block;")
   }
 })
+
+function valBoundInput(evt)
+{
+    //console.log(evt.target.value + "" + evt.key);
+    //console.log(evt.target.attributes.low);
+    //console.log(evt.target.attributes.high);
+    if (isNumberKey(evt) && inRange(evt.target.value + "" + evt.key, evt.target.attributes.low.value, evt.target.attributes.high.value)) { return true; }
+    else { return false; }
+}
+
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+function inRange(input, low, high){
+  if (low <= parseInt(input) && parseInt(input) <= high){
+    return true;
+  }
+  else{ return false; }
+}
